d9301d1a948592c77727ddd7266ec503ab762d6a
Natan Gomes <natan.vieira@itixti.com.br>
2023-09-11 10:35:18 -0300

    criado o método insert category

--- src/main/java/com/study/controllers/CategoryController.java
+++ src/main/java/com/study/controllers/CategoryController.java
@@ -1,13 +1,17 @@
 package com.study.controllers;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.study.dto.CategoryDTO;
 import com.study.services.CategoryService;
@@ -30,8 +34,19 @@ public class CategoryController {
     @GetMapping(value = "/{id}")
     public ResponseEntity<CategoryDTO> findById(@PathVariable Long id) {
         
-        CategoryDTO category = service.findById(id);
+        CategoryDTO dto = service.findById(id);
 
-        return ResponseEntity.ok().body(category);
+        return ResponseEntity.ok().body(dto);
     }
+
+    @PostMapping
+    public ResponseEntity<CategoryDTO> insert(@RequestBody CategoryDTO dto) {
+
+        dto = service.insert(dto);
+
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(dto.getId()).toUri();
+
+        return ResponseEntity.created(uri).body(dto);
+    }
+
 }
